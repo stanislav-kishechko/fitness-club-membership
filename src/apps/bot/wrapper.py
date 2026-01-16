663d71d1606c7b75cb0fcb0fad02e0ad62fa7a0e
Taras Ivashchuk <tvishy@disroot.org>
2026-01-16 14:57:24 +0200

    Removed unnecessary code in start handler. Changed m parameter to mes to provide readability

--- src/apps/bot/wrapper.py
+++ src/apps/bot/wrapper.py
@@ -15,11 +15,8 @@ class BaseBotWrapper:
 
     def register_handlers(self):
         @self.bot.message_handler(commands=["start"])
-        def command_start(mes):
-            cid = mes.chat.id
-            uid = mes.from_user.id
-            print("cid", cid)
-            print("uid", uid)
+        def command_start(mes):  # noqa
+            pass
 
     def run(self):
         self.bot.infinity_polling()
@@ -33,8 +30,8 @@ class SubscrUnsubscrBotWrapper(SubscriptionMixin, UnsubscriptionMixin, BaseBotWr
         super().register_handlers()
 
         @self.bot.message_handler(commands=["help"])
-        def command_help(m):
-            cid = m.chat.id
+        def command_help(mes):
+            cid = mes.chat.id
             help_text = "The following options are available: \n"
             for key in COMMANDS:
                 help_text += "/" + key + ": "
@@ -42,18 +39,17 @@ class SubscrUnsubscrBotWrapper(SubscriptionMixin, UnsubscriptionMixin, BaseBotWr
             self.bot.send_message(cid, help_text)
 
         @self.bot.message_handler(commands=["subscribe"])
-        def command_subscribe(m):
-            cid = m.chat.id
-            # todo
-            uid = m.from_user.id
+        def command_subscribe(mes):
+            cid = mes.chat.id
+            uid = mes.from_user.id
             self.subscribe(uid, cid)
 
         @self.bot.message_handler(commands=["unsubscribe"])
-        def command_unsubscribe(m):
-            cid = m.chat.id
-            uid = m.from_user.id
+        def command_unsubscribe(mes):
+            cid = mes.chat.id
+            uid = mes.from_user.id
             self.unsubscribe(uid, cid)
 
         @self.bot.message_handler(func=lambda _: True)
-        def command_default(m):
-            command_help(m)
+        def command_default(mes):
+            command_help(mes)
