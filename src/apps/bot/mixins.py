cad7cd66d0ee339651615f0a03aa42a1f8bc8dc3
Taras Ivashchuk <tvishy@disroot.org>
2026-01-16 14:59:27 +0200

    Added blank lines for better readibility

--- src/apps/bot/mixins.py
+++ src/apps/bot/mixins.py
@@ -11,6 +11,7 @@ class User:
 
 
 class SubscriptionMixin:
+
     def subscribe(self, uid, cid):
         text_success = (
             "Success! You'll receive notifications about: \n"
@@ -18,10 +19,10 @@ class SubscriptionMixin:
             "frozen membership\n"
             "success payments\n"
         )
-        self.bot.send_message(cid, text_success)
         text_already_subscribed = "You're already subscribed!"
         text_not_registered = "You're not registered yet!"
         usr = User(uid)
+
         if usr.exists():
             if usr.subscribed is not None:
                 self.bot.send_message(cid, text_already_subscribed)
@@ -30,15 +31,18 @@ class SubscriptionMixin:
             usr.save()
             self.bot.send_message(cid, text_success)
             return
+
         self.bot.send_message(cid, text_not_registered)
 
 
 class UnsubscriptionMixin:
+
     def unsubscribe(self, uid, cid):
         text_success = "You've succesfully unsubscribed"
         text_already_unsubscribed = "You're already unsubscribed!"
         text_not_registered = "You're not registered yet!"
         usr = User(uid)
+
         if usr.exists():
             if usr.subscribed is None:
                 self.bot.send_message(cid, text_already_unsubscribed)
@@ -47,4 +51,5 @@ class UnsubscriptionMixin:
             usr.save()
             self.bot.send_message(cid, text_success)
             return
+
         self.bot.send_message(cid, text_not_registered)
